09dcd8a3b58d46e4d3d4b45665e858e7c93abf8b
NISHANT KUMAR <99410372+nishu65@users.noreply.github.com>
2026-03-08 14:21:40 +0530

    Add solution for expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def expected_value_discrete(x, p):
+    """
+    Returns: float expected value
+    """
+    # Write code here
+    x = np.array(x)
+    p = np.array(p)
+
+    # check same length
+    if len(x) != len(p):
+        raise ValueError("x and p must have same length")
+
+    # check probability sum
+    if not np.isclose(np.sum(p), 1):
+        raise ValueError("Probabilities must sum to 1")
+
+    return np.sum(x * p)
+    pass
