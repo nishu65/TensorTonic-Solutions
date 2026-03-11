2fe1356807276171ca843ab80d0ae877749e79ca
NISHANT KUMAR <99410372+nishu65@users.noreply.github.com>
2026-03-11 11:46:07 +0530

    Add solution for popularity-ranking

---
+++ popularity-ranking/popularity-ranking.py
@@ -0,0 +1,14 @@
+def popularity_ranking(items, min_votes, global_mean):
+    """
+    Compute the Bayesian weighted rating for each item.
+    """
+    # Write code here
+    result = []
+    
+    for avg_rating, num_votes in items:
+        wr = (num_votes/(num_votes + min_votes))*avg_rating + \
+             (min_votes/(num_votes + min_votes))*global_mean
+        
+        result.append(wr)
+    
+    return result
