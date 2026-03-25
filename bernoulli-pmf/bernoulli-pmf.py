a2704ffb659bd2054589c941ff8789067467a486
NISHANT KUMAR <99410372+nishu65@users.noreply.github.com>
2026-03-25 13:05:27 +0530

    Add solution for bernoulli-pmf

---
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def bernoulli_pmf_and_moments(x, p):
+    """
+    Compute Bernoulli PMF and distribution moments.
+    """
+    # Write code here
+    x=np.array(x)
+    pmf=np.where(x==1,p,1-p)
+    mean=p
+    var=p*(1-p)
+    return pmf,float(mean),float(var)
+    pass
